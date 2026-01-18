a7060271402838f8a821133d60a9e9bdea873249
Ivhuredu <belovedwafaith@outlook.com>
2026-01-18 15:52:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -45,6 +45,7 @@ def init_db():
     conn.commit()
     conn.close()
     
+
 def init_module_access_table():
     conn = get_db()
     c = conn.cursor()
@@ -408,6 +409,7 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 
 
 
