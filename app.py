c0a0db598767c6268e5f34927ee8b45a0bba9918
Ivhuredu <belovedwafaith@outlook.com>
2026-07-30 18:33:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7541,7 +7541,11 @@ def test_template():
     return "Template sent"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+    app.run(
+        host=HOST,
+        port=PORT,
+        debug=DEBUG
+    )
 
 
 
